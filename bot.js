c895c4c02b55dc07a78e5f8fc559982f1b69db60
Jessica <jessicahahm@gmail.com>
2025-09-07 12:00:38 -0300

    server added

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+require("./server"); // inicia o server express
+
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
