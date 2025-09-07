5ee90e3278dd25b3cdc0428d28fea9d3880e8510
Jessica <jessicahahm@gmail.com>
2025-09-07 12:15:46 -0300

    server removed

--- bot.js
+++ bot.js
@@ -1,5 +1,3 @@
-require("./server"); // inicia o server express
-
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
